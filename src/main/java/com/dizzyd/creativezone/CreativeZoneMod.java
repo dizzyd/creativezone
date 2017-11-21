3d355e0086f72c8edc8e9fc62571166a46feca8c
Dave Smith <dizzyd@dizzyd.com>
2017-11-21 14:32:53 -0700

    Bump to 1.0.2

--- src/main/java/com/dizzyd/creativezone/CreativeZoneMod.java
+++ src/main/java/com/dizzyd/creativezone/CreativeZoneMod.java
@@ -35,15 +35,13 @@ import java.time.Instant;
 import java.util.HashSet;
 
 @Mod(modid = CreativeZoneMod.MODID, version = CreativeZoneMod.VERSION)
-public class CreativeZoneMod
-{
+public class CreativeZoneMod {
     public static final String MODID = "creativezone";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     static int checkInterval;
     static int zoneRadius;
     static HashSet<String> whitelist = new HashSet<>();
-
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
