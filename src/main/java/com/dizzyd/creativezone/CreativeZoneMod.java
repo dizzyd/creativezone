2a6ec3c2cd65b4018ad14da2959444fb32eceab2
Dave Smith <dizzyd@dizzyd.com>
2017-11-20 21:06:24 -0700

    Bump to 1.0.1; add support for whitelist

--- src/main/java/com/dizzyd/creativezone/CreativeZoneMod.java
+++ src/main/java/com/dizzyd/creativezone/CreativeZoneMod.java
@@ -22,6 +22,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -30,15 +31,17 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import java.io.File;
 import java.time.Instant;
+import java.util.HashSet;
 
 @Mod(modid = CreativeZoneMod.MODID, version = CreativeZoneMod.VERSION)
 public class CreativeZoneMod
 {
     public static final String MODID = "creativezone";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     static int checkInterval;
     static int zoneRadius;
+    static HashSet<String> whitelist = new HashSet<>();
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
@@ -47,12 +50,18 @@ public class CreativeZoneMod
         config.load();
 
         // Check interval (seconds)
-        checkInterval = config.getInt("ScanInterval", "", 1, 1, 60,
+        checkInterval = config.getInt("ScanInterval", "config", 1, 1, 60,
                 "Sets the interval (in seconds) for scanning player locations");
 
         // Creative zone radius
-        zoneRadius = config.getInt("ZoneRadius", "", 25, 5, 1000,
+        zoneRadius = config.getInt("ZoneRadius", "config", 25, 5, 1000,
                 "Sets the radius of the creative zone");
+
+        Property whiteListProp = config.get("config", "Whitelist", new String[0],
+                "Gets the list of whitelisted users");
+        for (String s : whiteListProp.getStringList()) {
+            whitelist.add(s);
+        }
 
         config.save();
     }
@@ -75,10 +84,15 @@ public class CreativeZoneMod
                     for (int i = 0; i < e.world.playerEntities.size(); i++)
                     {
                         EntityPlayer p = e.world.playerEntities.get(i);
+                        // If the user is inside the zone radius, force them back to creative
                         if (p.getDistance(spawn.getX(), p.posY, spawn.getZ()) < zoneRadius) {
                             p.setGameType(GameType.CREATIVE);
                         } else {
-                            p.setGameType(GameType.SURVIVAL);
+                            // Otherwise, the user is outside the radius and we need to force
+                            // them back to survival (assuming they're not on the whitelist)
+                            if (!whitelist.contains(p.getName())) {
+                                p.setGameType(GameType.SURVIVAL);
+                            }
                         }
                     }
                     lastCheck = now;
